59adc0c8f290b5158b5612b15d6de0a3e4b61192
TriTue2011 <tritue0610@gmail.com>
2026-05-12 14:58:07 +0700

    fix: use pre-built image, remove Dockerfile
    HA Supervisor pulls image directly from ghcr.io/tritue2011/chatgpt2api:latest

--- chatgpt2api/run.sh
+++ chatgpt2api/run.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-
-# Read auth key from HA options (passed as env CHATGPT2API_AUTH_KEY)
-# HA Supervisor automatically maps addon options to environment variables
-
+cd /app
 exec uv run uvicorn main:app --host 0.0.0.0 --port 3030
